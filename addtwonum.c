e68ae47cb164ca0c24f37e551348e9389dac6f9b
mdbiplobmia <46644708+mdbiplobmia@users.noreply.github.com>
2019-01-13 23:45:26 +0600

    Add files via upload

---
+++ addtwonum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int a,b,s;
+    /* Input numbers from user */
+    printf("Enter value of a & b: ");
+    scanf("%d%d",&a,&b);
+    s=a+b;
+
+    printf("%d+%d=%d\n",a,b,s);
+}
